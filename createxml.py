fd9e24764693bb6e19674ecbe0023f9390823fd3
Sean Custer <34922998+sean-custer@users.noreply.github.com>
2021-11-30 12:39:07 -0500

    Update createxml.py

--- createxml.py
+++ createxml.py
@@ -7,56 +7,60 @@ import json
 
 if __name__ == '__main__':
     # Use metadata to query Azure & parse the Json file to get the resource group of this VM
-    x =  subprocess.check_output('curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"', shell=True)
+    x =  subprocess.check_output('curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" | jq .', shell=True)
     x = x.decode("utf-8")
     keyVal = 'resourceGroupName'
     resourceGroupName = ''
     toParse = json.loads(x)
 
-    #:print(toParse['compute']['resourceGroupName'])
+    print(toParse['compute']['resourceGroupName'])
 
     if 'compute' in toParse:
         if keyVal in toParse['compute']:
             resourceGroupName = toParse['compute'][keyVal]
 
-
-
-    txt = '''<user-mapping>
+ 
+    headerTxt = '<user-mapping>\n'
+    txt = '''
                 <authorize
                         username="arsiem"
                         password="Arsiem2020!!">
-                    <connection name="Ubuntu-Server-SSH">
+                    <connection name="Default-Server-SSH">
                         <protocol>ssh</protocol>
                         <param name="hostname"></param>
                         <param name="port">22</param>
                     </connection>
-
-                    <connection name="Ubuntu-Server-RDP">
+                    <connection name="Default-Server-RDP">
                         <protocol>rdp</protocol>
                         <param name="hostname"></param>
                     <param name="port">3389</param>
                     </connection>
                 </authorize>
-            </user-mapping>'''
+            '''
+    closerTxt = '</user-mapping>'
 
-
-
+    result = headerTxt
     cwd = os.getcwd()
     # LOAD XSL SCRIPT
     x = requests.get('https://api-for-guacips.azure-api.net/manual/paths/invoke?name=%s' %resourceGroupName)
     ipdata = x.json()
-    print(ipdata)
-    xml = et.fromstring(txt)
-    xsl = et.parse('XSLTScript.xsl')
-    transform = et.XSLT(xsl)
+    for i in range(len(ipdata)):
+        if 'guac' not in ipdata[i]:
+            # FORMAT STRING VALUE
+            x = ipdata[i].split('|')
+            print(x)
+            newList = x[0].split('-')
+            name = newList[0].strip()
+            ip = x[1]
+            data = et.XSLT.strparam(ip.strip())
 
-    # PASS PARAMETER TO XSLT
-    n = et.XSLT.strparam(ipdata[0])
-    result = transform(xml, new_ip=n)
+            xml = et.fromstring(txt.replace('Default', name))
+            xsl = et.parse('XSLTScript.xsl')
+            transform = et.XSLT(xsl)
 
-    print(result)
-
-
+            # PASS PARAMETER TO XSLT            
+            result += str(transform(xml, new_ip=data))
+    result += closerTxt
     # SAVE XML TO FILE
-    with open('user-mapping.xml', 'wb') as f:
+    with open('user-mapping.xml', 'w') as f:
         f.write(result)
